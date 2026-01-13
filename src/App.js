d4c5dff9a5f57b4387039a79fa2a78cdfde35177
furstcontact <furstcontact@gmail.com>
2026-01-13 20:21:38 +1030

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather React App</h1>
-      <footer>
-        This project was coded by Tanya Furst and is open-sourced on{" "}
-        <a
-          href="https://github.com/furstcontact/weather-react-app"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather React App</h1>
+        <footer>
+          This project was coded by Tanya Furst and is open-sourced on{" "}
+          <a
+            href="https://github.com/furstcontact/weather-react-app"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
