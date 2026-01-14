7e9818e6af769d2828b5a696499fe4a75191263f
furstcontact <furstcontact@gmail.com>
2026-01-14 16:18:30 +1030

    added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import "./App.css";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather React App</h1>
         <Weather />
         <footer>
           This project was coded by Tanya Furst and is open-sourced on{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,51 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Fuck this</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Budapest</h1>
+      <ul>
+        <li>Wednesday 12:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row mt-4">
+        <div className="col-6">
+          <img
+            src="https://www.gstatic.com/weather/conditions/v3/svg/cloudy_light.svg"
+            alt="cloudy"
+          />
+          <span className="temperature">-6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 10%</li>
+            <li>Humidity: 92%</li>
+            <li>Wind: 10 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
